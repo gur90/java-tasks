bca4248d0dc31f248ad3a30fc74e2e4434621832
gur90 <95541028+gur90@users.noreply.github.com>
2022-08-11 10:56:32 +0300

    first lesson

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        showInfo();
+        callMe();
+    }
+    public static void showInfo(){
+        System.out.println("look at this info");
+    }
+    public static void callMe(){
+        System.out.println("please, call me 123456");
+    }
+}
