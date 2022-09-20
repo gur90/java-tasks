505fa1bfbe81fe536c2052ff33b52d777924c9cc
gur90 <annettgur@gmail.com>
2022-09-20 22:15:56 +0300

    fix: fix methods

--- lesson10/src/HomeTask10.java
+++ lesson10/src/HomeTask10.java
@@ -8,27 +8,20 @@ public class HomeTask10 {
         System.out.println(teenNumbers(13, 20, 10));//true
         System.out.println(teenNumbers(20, 19, 10));//true
         System.out.println(teenNumbers(20, 10, 13));//true
+        System.out.println(teenNumbers(9, 6, 4));
     }
 
     public static boolean startHi(String str) {
         String input = str.substring(0, 2);
-        System.out.println(input);
-        switch (input) {
-            case "hi":
-                return true;
-            default:
-                return false;
-
+        if (input == "hi") {
+            return true;
+        } else {
+            return false;
         }
+
     }
 
     public static boolean teenNumbers(int one, int two, int three) {
-        boolean answer = false;
-        if ((one <= 19 && one >= 13) || (two <= 19 && two >= 13) || (three <= 19 && three >= 13)) {
-            answer = true;
-        } else {
-            answer = false;
-        }
-        return answer;
+        return ((one <= 19 && one >= 13) || (two <= 19 && two >= 13) || (three <= 19 && three >= 13));
     }
 }
