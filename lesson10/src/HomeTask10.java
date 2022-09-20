68057941db2a721519493bb1c7568d648d8b5e78
gur90 <annettgur@gmail.com>
2022-09-20 22:38:04 +0300

    feat: add decision

--- lesson10/src/HomeTask10.java
+++ lesson10/src/HomeTask10.java
@@ -1,9 +1,9 @@
 
 public class HomeTask10 {
     public static void main(String[] args) {
-        System.out.println(startHi("hi there")); //true
-        System.out.println(startHi("hi"));// true
-        System.out.println(startHi("hello hi"));// false
+        System.out.println(startHiFirst("hi there")); //true
+        System.out.println(startHiFirst("hi"));// true
+        System.out.println(startHiFirst("hello hi"));// false
         System.out.println("----------------");
         System.out.println(teenNumbers(13, 20, 10));//true
         System.out.println(teenNumbers(20, 19, 10));//true
@@ -11,17 +11,20 @@ public class HomeTask10 {
         System.out.println(teenNumbers(9, 6, 4));
     }
 
-    public static boolean startHi(String str) {
+    public static boolean startHiFirst(String str) {
         String input = str.substring(0, 2);
-        if (input == "hi") {
+        if (input.equals("hi") ) {
             return true;
         } else {
             return false;
         }
 
     }
+    public static boolean startHiSecond(String str){
+        return str.startsWith("hi");
+    }
 
     public static boolean teenNumbers(int one, int two, int three) {
-        return ((one <= 19 && one >= 13) || (two <= 19 && two >= 13) || (three <= 19 && three >= 13));
+        return (one <= 19 && one >= 13) || (two <= 19 && two >= 13) || (three <= 19 && three >= 13);
     }
 }
