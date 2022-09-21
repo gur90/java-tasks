02187d6e5b126ea61d415b996fb54c3de63e0bdb
gur90 <annettgur@gmail.com>
2022-09-21 10:17:44 +0300

    feat: done first 5 tasks

--- lesson10/src/LogicalTasks.java
+++ lesson10/src/LogicalTasks.java
@@ -11,6 +11,18 @@ public class LogicalTasks {
         System.out.println(caughtSpeeding(60, false));// 0
         System.out.println(caughtSpeeding(65, false));// 1
         System.out.println(caughtSpeeding(65, true));// 0
+        System.out.println("---------");
+        System.out.println(love6(6, 4)); // true
+        System.out.println(love6(4, 5)); // false
+        System.out.println(love6(1, 5)); // true
+        System.out.println("--------");
+        System.out.println(more20(20));//false
+        System.out.println(more20(21));// true
+        System.out.println(more20(42));//true
+        System.out.println("----------");
+        System.out.println(nearTen(12));// true
+        System.out.println(nearTen(17)); // false
+        System.out.println(nearTen(19));// true
     }
 
     public static boolean squirrelsParty(int cigars, boolean weekend) {
@@ -28,13 +40,25 @@ public class LogicalTasks {
     }
 
     public static int caughtSpeeding(int speed, boolean birthday) {
-        if (speed <= 60 && !birthday || speed<=65 && birthday) {
+        if (speed <= 60 && !birthday || speed <= 65 && birthday) {
             return 0;
-        } else if (speed <= 80 && !birthday || speed<=85 && birthday) {
+        } else if (speed <= 80 && !birthday || speed <= 85 && birthday) {
             return 1;
         } else {
             return 2;
         }
 
+    }
+
+    public static boolean love6(int a, int b) {
+        return a == 6 || b == 6 || Math.abs(a - b) == 6 || Math.abs(a + b) == 6;
+    }
+
+    public static boolean more20(int number) {
+        return number % 20 == 1 || number % 20 == 2;
+    }
+
+    public static boolean nearTen(int num) {
+        return num % 10 <= 2 || num % 10 >= 8;
     }
 }
