83ab85fb554559a52b15810dfde8fd46cb9bf5ce
gur90 <annettgur@gmail.com>
2022-09-30 23:09:48 +0300

    feat: debug

--- lesson-12/src/HomeTask.java
+++ lesson-12/src/HomeTask.java
@@ -1,12 +1,12 @@
 public class HomeTask {
     public static void main(String[] args) {
-        simpleNumber(6);
+        simpleNumber(8);
     }
 
     public static void simpleNumber(int number) {
         for (int i = 2; i < number; i++) {
             if (number % i == 0) {
-                System.out.println("it is not simple");
+                System.out.println("it is not simple " + i);
                 return;
             }
         }
