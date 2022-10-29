00cb13240476fc4a737302547d4195b7db662605
gur90 <annettgur@gmail.com>
2022-10-29 14:46:00 +0300

    hw

--- src/Juliy.java
+++ src/Juliy.java
@@ -1,7 +1,53 @@
 public class Juliy {
     public static void main(String[] args) {
-        enigmaCaesar("Съешь же ещё этих мягких французских булок, да выпей чаю.");
-        //"Фэзыя йз зьи ахлш пвёнлш чугрщцкфнлш дцосн, жг еютзм ъгб."
+        allCezarDecision();
+    }
+
+    public static void allCezarDecision() {
+        String etalon = "Фэзыя йз зьи ахлш пвёнлш чугрщцкфнлш дцосн, жг еютзм ъгб.";
+        String input = "Съешь же ещё этих мягких французских булок, да выпей чаю.";
+        System.out.println(input);
+        System.out.println(codedInput(input));
+        System.out.println(etalon);
+    }
+
+    private static String problemChars(char symbol) {
+        char result = (char) (symbol + 3);
+        if (symbol == ' ') {
+            result = ' ';
+        } else if (symbol == '.') {
+            result = '.';
+        } else if (symbol == ',') {
+            result = ',';
+        } else if (symbol == '!') {
+            result = '!';
+        } else if (symbol == 'э') {
+            result = 'а';
+        } else if (symbol == 'ю') {
+            result = 'б';
+        } else if (symbol == 'я') {
+            result = 'в';
+        } else if (symbol == 'е') {
+            result = 'з';
+        } else if (symbol == 'г') {
+            result = 'е';
+        } else if (symbol == 'д') {
+            result = 'ж';
+        }
+        return String.valueOf(result);
+    }
+
+    private static String changeChars(String input) {
+        String output = "";
+        for (int i = 0; i < input.length(); i++) {
+            output = output + problemChars(input.charAt(i));
+        }
+        return output;
+    }
+
+    public static String codedInput(String input) {
+        String newInput = String.valueOf(changeChars(input));
+        return newInput;
     }
 
     public static void enigmaCaesar(String input) {
@@ -20,5 +66,4 @@ public class Juliy {
             }
         }
     }
-
 }
