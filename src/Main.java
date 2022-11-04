25287706541e60fdc9a43ab9200d162f9f977cd3
gur90 <annettgur@gmail.com>
2022-11-04 15:28:17 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,58 @@
 public class Main {
+    public static void main(String[] args) {
+        dinner();
+        takeASip(24);
+    }
+
+    private static void takeASip(int button) {
+        switch (button) {
+            case 1:
+                System.out.println("Take your tea, please");
+                break;
+            case 2:
+                System.out.println("Your cofee, nice man!");
+                break;
+            case 3:
+                System.out.println("Would you like to drink some wine? Please, keep calm and take a sip!");
+                break;
+            case 4:
+                System.out.println("It is a nice day to drink latte!");
+                break;
+            case 5:
+                System.out.println("We cooked delicious cappuccino for you!");
+                break;
+            default:
+                System.out.println("Error! try again, please!");
+                break;
+        }
+    }
+
+    public static void dinner() {
+        int guestNumber = 13;
+        switch (guestNumber) {
+            case 0:
+                System.out.println("I eat alone, it so sad");
+                break;
+            case 1:
+                System.out.println("We will eat salade. " +
+                        "And maybe drink some wine");
+                break;
+            case 2:
+                System.out.println("Let's cook some pasta");
+                break;
+            case 3:
+                System.out.println("We will cook a duck");
+                break;
+            case 7:
+                System.out.println("let's go to the church");
+                break;
+            case 13:
+                System.out.println("I will cook a black rooster");
+                break;
+            default:
+                System.out.println("Lets go to the restaurant");
+                break;
+        }
+
+    }
 }
