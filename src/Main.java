638466f3241323ee0709ad9dcacf31c5122cca50
gur90 <annettgur@gmail.com>
2022-11-08 17:38:39 +0300

    feat: add

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,22 @@ public class Main {
     /* Given a string, compute recursively (no loops) a new string where all appearances of “pi” have been replaced by “3.14”.
      Получив строку, вычислите рекурсивно (без циклов) новую строку, в которой все появления “pi” были заменены на “3.14”.*/
     public static void main(String[] args) {
+
+
+
         System.out.println(changePi("xpix"));// → "x3.14x"
         System.out.println(changePi("pipi"));// → "3.143.14"
         System.out.println(changePi("pip"));// → "3.14p"
+    }
+
+
+
+    private static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
     }
 
     private static String changePi(String str) {
@@ -17,4 +30,5 @@ public class Main {
         }
         return "err";
     }
+
 }
