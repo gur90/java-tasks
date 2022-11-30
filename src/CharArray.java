75e24a153ff79f4104db174ecb7f3654091a00ac
gur90 <annettgur@gmail.com>
2022-11-30 14:17:52 +0300

    home work mix array

--- src/CharArray.java
+++ src/CharArray.java
@@ -2,17 +2,28 @@ import java.util.Arrays;
 
 public class CharArray {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(createArray()));
+        System.out.println(Arrays.toString(mixArray(createArray())));
+    }
+
+    private static char[] mixArray(char[]input) {
+        char[]output=new char[input.length];
+        int min =1;
+        int max= input.length-1;
+        for (int i = 0; i < output.length; i++) {
+            int random = min + (int)(Math.random()*max);
+            output[i]=input[random];
+        }
+        return output;
     }
 
     private static char[] createArray() {
-        char[] output = new char[36];
-        int num =65t;
-        for (int i = 0; i < 36; i++) {
+        char[] output = new char[26];
+        int num =65;
+        for (int i = 0; i < 26; i++) {
             output[i] = (char) num;
             num++;
         }
-
         return output;
     }
 }
+
